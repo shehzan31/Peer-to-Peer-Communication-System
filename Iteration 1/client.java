8bc87c2fbe6c195f7f427a249f9e1587f7f87698
HumbleChaudhry <humblechaudhry@live.ca>
2022-01-24 18:08:37 -0700

    Update client.java

--- Iteration 1/client.java
+++ Iteration 1/client.java
@@ -1,12 +1,19 @@
 import java.io.*;
 import java.net.*;
 
+
 public class client {
 
     public static void sendTeamName(BufferedWriter writer){
         String teamName = "The Social Network";
-        writer.write(teamName+"/n");
-        writer.flush();
+
+        try{
+            writer.write(teamName+"/n");
+            writer.flush();
+        }
+        catch(Exception err) {
+            System.out.println("Error: " + err.getMessage());
+            }
     }
 
     public static void sendCode(BufferedWriter writer){
@@ -22,50 +29,51 @@ public class client {
     }
     
     
-    public static void main(String[] args) {
-        String host = "localhost";
-        String portNum = 55921;
-        
-        try{
-            Socket clientSocket = new clientSocket(host, portNum);
-            BufferedReader reader = new BufferedReader(clientSocket.getInputStream());
-            BufferedWriter writer = new BufferedWriter(clientSocket.getOutputStream());
+    public static void main(String[] args)
+	{
+
+		try (
+				Socket clientSocket = new Socket("localhost", 55921);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+				
+			)
+		{
             Boolean open = true;
-            while(open){
-                String request;
-                request = reader.readLine().trim();
-                switch(request){
-                    case "get team name":
-                        System.out.println("Requesting team name");
-                        sendTeamName(writer);
-                        System.out.println("Sent team name");
-                        break;
-                    case "get code":
-                        System.out.println("Requesting code base");
-                        sendCode(writer);
-                        System.out.println("Sent code base");
-                        break;
-                    case "receive request":
-                        System.out.println("Receiving request");
-                        receiveInfo(request);
-                        System.out.println("Received");
-                        break;
-                    case "get report":
-                        System.out.println("Requesting report");
-                        sendReport(writer);
-                        System.out.println("Sent report");
-                        break;
-                    case "close":
-                        System.out.println("Requesting to close the socket");
-                        open = false;
-                        break;
-                }
-            }
-            clientSocket.close();
-            System.out.println("Closed the socket");
-        }
-        catch(SocketException e){
-            System.out.println("Ran into some error");
-        }
-    }
+                    while(open){
+                        String response;
+                        response = reader.readLine().trim();
+                        switch(response){
+                            case "get team name":
+                                System.out.println("Requesting team name");
+                                sendTeamName(writer);
+                                System.out.println("Sent team name");
+                                break;
+                            case "get code":
+                                System.out.println("Requesting code base");
+                                sendCode(writer);
+                                System.out.println("Sent code base");
+                                break;
+                            case "receive request":
+                                System.out.println("Receiving request");
+                                receiveInfo(response);
+                                System.out.println("Received");
+                                break;
+                            case "get report":
+                                System.out.println("Requesting report");
+                                sendReport(writer);
+                                System.out.println("Sent report");
+                                break;
+                            case "close":
+                                System.out.println("Requesting to close the socket");
+                                open = false;
+                                break;
+                        }
+                    }
+
+		}
+		catch(Exception err) {
+			System.out.println("Error: " + err.getMessage());
+		}
+	}
 }
